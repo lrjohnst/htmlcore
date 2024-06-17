251af395e169b590d0b6aad08e542828d4fe6e96
Lucas Johnston <lrjohnston@live.nl>
2024-06-17 20:00:15 +0200

    v1.0.1 [Untracked all view files]

--- resources/views/pages/artikelen.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>Artikelen</h1>
-<p>Naar de <a href="/en/articles">Engelse artikelenpagina</a>.</p>
-<p>Naar de <a href="/">Voorpagina</a>.</p>
--- resources/views/pages/voorpagina_nl.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>Voorpagina</h1>
-<p>Naar de <a href="/en">Engelse voorpagina</a>.</p>
-<p>Naar de <a href="/nl/artikelen">Artikelen pagina</a>.</p>
